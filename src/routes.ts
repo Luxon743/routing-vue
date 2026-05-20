cbb490a00683f4d56a8fad8b582505416172979b
Lucciano Perticarini <lucciperti7@gmail.com>
2026-05-20 18:45:18 -0300

    build: Create proyecto base;

---
+++ src/routes.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
+import HomeView from "./views/HomeView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: '',
+        component: HomeView
+    },
+    {
+        path: '/users',
+        name: 'user',
+        component: () => import('./views/UserListVue.vue')
+    },
+    {
+        path: '/users/:id',
+        name: 'userview',
+        component: () => import ('./views/UserView.vue')
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+});
+
+export default router;
